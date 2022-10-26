40edc77e7880f00a27e37f484e36cc4f5fd2b460
panquequelol <rene@outlook.my>
2022-10-25 23:08:37 -0300

    feat: add context provider

---
+++ src/components/search/Provider.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import { InstantSearch } from "react-instantsearch-hooks-web";
+
+interface ProviderProps {
+  children: ReactNode;
+  indexName: string;
+  searchClient: any;
+}
+
+function Provider({ children, searchClient, indexName }: ProviderProps) {
+  return (
+    <InstantSearch searchClient={searchClient} indexName={indexName}>
+      {children}
+    </InstantSearch>
+  );
+}
+
+export default Provider;
--- src/search.ts
+++ src/search.ts
@@ -1,6 +1,7 @@
 import SearchFilter from "./components/search/filters/SearchFilter";
 import SearchSortBy from "./components/search/filters/SearchSortBy";
+import Provider from "./components/search/Provider";
 import SearchBar from "./components/search/SearchBar";
 
 
-export { SearchBar, SearchFilter, SearchSortBy };
+export { SearchBar, SearchFilter, SearchSortBy, Provider };
