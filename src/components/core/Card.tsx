9b4fad28770778745c3b8e8a865d1001b72daa43
panquequelol <rene@outlook.my>
2022-11-06 21:20:29 -0300

    style(card): space between title and body

--- src/components/core/Card.tsx
+++ src/components/core/Card.tsx
@@ -7,8 +7,8 @@ interface CardProps {
 
 function Card({ title, children }: CardProps) {
   return (
-    <article className="flex flex-col rounded-2xl border border-grayscale-4 bg-grayscale-2 p-4 shadow">
-      <h2 className="text-xl font-extrabold">{title}</h2>
+    <article className="flex w-full flex-col rounded-2xl border border-grayscale-4 bg-grayscale-2 p-4 shadow">
+      <h2 className="mb-2 text-xl font-extrabold">{title}</h2>
       <div className="flex-auto">{children}</div>
     </article>
   );
