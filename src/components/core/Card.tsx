2a9a50a21fb591464dd99b859716a22a04229e8b
panquequelol <rene@outlook.my>
2022-11-10 00:31:42 -0300

    chore(card): make title optional

--- src/components/core/Card.tsx
+++ src/components/core/Card.tsx
@@ -1,14 +1,14 @@
 import { ReactNode } from "react";
 
 interface CardProps {
-  title: string;
+  title?: string;
   children: ReactNode;
 }
 
 function Card({ title, children }: CardProps) {
   return (
     <article className="flex w-full flex-col rounded-2xl border border-grayscale-4 bg-grayscale-2 p-4 shadow">
-      <h2 className="mb-2 text-xl font-extrabold">{title}</h2>
+      {title && <h2 className="mb-2 text-xl font-extrabold">{title}</h2>}
       <div className="flex-auto">{children}</div>
     </article>
   );
